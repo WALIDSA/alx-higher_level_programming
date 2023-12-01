07276e8aa1fd892a3635962ac82cacb5692b214d
walid salmi <salamiwalid1999@gmail.com>
2023-12-01 12:12:42 +0000

    tasks

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """
-- displays the body of the response (decoded in utf-8).
+given URL, sends a request, displays response (utf-8).
+usage: ./3-error_code.py http://0.0.0.0:5000/status_401
 """
+import urllib.request
+from sys import argv
+import urllib.parse
 
 
 if __name__ == "__main__":
-    import sys
-    from urllib import request, error
-
     try:
-        with request.urlopen(sys.argv[1]) as res:
-            print(res.read().decode('UTF-8'))
-    except error.HTTPError as er:
-        print('Error code:', er.code)
-
+        with urllib.request.urlopen(argv[1]) as response:
+            print(response.read().decode())
+    except urllib.error.URLError as e:
+        print("Error code: {}".format(e.code))
